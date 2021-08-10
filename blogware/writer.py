228288b9c86a8119a94dc3ad4072ba9eee03f46e
Anton Schwarz <anton@mendoza-schwarz.de>
2021-08-10 15:48:45 +0200

    main writer

--- blogware/writer.py
+++ blogware/writer.py
@@ -1,7 +1,41 @@
 """
 Contains the methods to actually write the files to disk
 """
+from datetime import datetime
 
-def writeMain(loc) -> None:
-    with open(loc + "main.html", "w") as f:
-        f.write("Henlo (:")
+
+class Writer:
+    def __init__(self, loc, title) -> None:
+        self.loc = loc
+        self.title = title
+
+        self.mainloc = loc + "main.html"
+
+    def writeMain(self) -> None:
+        start = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{self.title}</title>
+    <style>
+    body {{
+        width: 60%;
+        margin: auto;
+    }}
+    </style>
+</head>
+<body>    
+    """
+        toppart = f"""
+<h1>{self.title}</h1>
+<p>Last updated {datetime.now().strftime("%d.%m.%Y %H:%M")}
+<hr>
+"""
+        end = f"""
+</body>
+</html>
+    """
+        with open(self.mainloc, "w") as f:
+            f.write(start)
+            f.write(toppart)
+            f.write(end)
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ This is the main script to be executed.
 It calls on the blogware package for the methods.
 """
 
-from blogware import writer
+from blogware.writer import Writer
 
 LOCATION = "output/"
+TITLE = "NBlog"
 
 if __name__ == "__main__":
     print("running Nblog!")
-    writer.writeMain(LOCATION)
+    writer = Writer(LOCATION, TITLE)
+    writer.writeMain()
